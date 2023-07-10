2d3face96967ea5d4a97f95910e6b7867388918c
Gabriel  Mwasaru  Mafura <mafurag@hotmail.com>
2023-07-10 13:44:00 +0300

    Check GP_GIT_SITE_PATH

--- .gpconfig/postdeploy.sh
+++ .gpconfig/postdeploy.sh
@@ -6,9 +6,11 @@ echo "Create testfolder"
 
 mkdir ../testfoldersys
 
-echo "Create testfolder2sys"
+echo "Create testfolder2sys "
 
 sudo mkdir -p ../../public/testfolder2sys
+
+echo " GP_GIT_SITE_PATH - ${GP_GIT_SITE_PATH}"
 
 echo "Post deploy make dir in current folder"
 
