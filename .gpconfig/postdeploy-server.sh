3adf7badada9b62eedf28fdea3307fa9725b6fbe
Gabriel  Mwasaru  Mafura <mafurag@hotmail.com>
2023-07-10 10:57:39 +0300

    testfolder2

--- .gpconfig/postdeploy-server.sh
+++ .gpconfig/postdeploy-server.sh
@@ -4,7 +4,13 @@ echo "This is an example post-deploy script. Running as root user"
 
 echo "Post deploy change 1"
 
+echo "Create testfolder
+
 mkdir ../testfolder
+
+echo "Create testfolder2"
+
+mkdir -p ../../public/testfolder2
 
 echo "Post deploy make dir in current folder"
 
