651f3e649459b1d90773fcb4de00b3703ddb1a29
Gabriel  Mwasaru  Mafura <mafurag@hotmail.com>
2023-07-10 10:29:01 +0300

    add mkdir temp

--- .gpconfig/postdeploy-server.sh
+++ .gpconfig/postdeploy-server.sh
@@ -3,3 +3,9 @@
 echo "This is an example post-deploy script. Running as root user"
 
 echo "Post deploy change 1"
+
+mkdir ../testfolder
+
+echo "Post deploy make dir in current folder"
+
+mkdir testdir
