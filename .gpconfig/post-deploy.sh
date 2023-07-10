2e24377069b5b6b0fd763406247d80e4628880be
Gabriel  Mwasaru  Mafura <mafurag@hotmail.com>
2023-07-10 11:06:46 +0300

    create folder from system user

--- .gpconfig/post-deploy.sh
+++ .gpconfig/post-deploy.sh
@@ -2,4 +2,14 @@
 
 echo "This is an example post-deploy script. Run as site system user"
 
+echo "Create testfolder"
+
+mkdir ../testfoldersys
+
+echo "Create testfolder2sys"
+
+mkdir -p ../../public/testfolder2sys
+
+echo "Post deploy make dir in current folder"
+
 
