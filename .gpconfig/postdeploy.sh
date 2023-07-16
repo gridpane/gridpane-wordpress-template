a4fd4ff5adfbb9785d774e01139108b9308201eb
Gabriel  Mwasaru  Mafura <mafurag@hotmail.com>
2023-07-16 16:39:29 +0300

    removed sudo

--- .gpconfig/postdeploy.sh
+++ .gpconfig/postdeploy.sh
@@ -8,7 +8,7 @@ mkdir ../testfoldersys
 
 echo "Create testfolder2sys "
 
-sudo mkdir -p ../../public/testfolder2sys
+#sudo mkdir -p ../../public/testfolder2sys
 
 echo " GP_GIT_SITE_PATH - ${GP_GIT_SITE_PATH}"
 
