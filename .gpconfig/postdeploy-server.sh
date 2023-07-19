882d496b2d9d4b8f5574ecda205657f08a1e600d
Gabriel  Mwasaru  Mafura <mafurag@hotmail.com>
2023-07-19 13:54:51 +0300

    post deploy with parent dir check

--- .gpconfig/postdeploy-server.sh
+++ .gpconfig/postdeploy-server.sh
@@ -2,22 +2,6 @@
 
 echo "This is an example post-deploy script. Running as root user"
 
-# echo "Post deploy change 1"
-
-# echo "Create testfolder"
-
-# mkdir ../testfolder
-
-# echo "Create testfolder2"
-
-# mkdir -p ../../public/testfolder2
-
-# echo " GP_GIT_SITE_PATH - ${GP_GIT_SITE_PATH}"
-
-# echo " GP_GIT_RELEASE_PATH - ${GP_GIT_RELEASE_PATH}"
-
-# echo "Post deploy make dir in current folder"
-
 # Server wide access
 # - Runs after files have been deployed 
 # - Runs as root
@@ -25,10 +9,17 @@ echo "This is an example post-deploy script. Running as root user"
 
 echo "$GP_GIT_RELEASE_PATH/release.path.postdeploy.server"
 touch "$GP_GIT_RELEASE_PATH/release.path.postdeploy.server"
+echo "Checking Parent folder - ${GP_GIT_RELEASE_PATH}"
+ls -l  "$GP_GIT_RELEASE_PATH"
+
 
 echo "$GP_GIT_SITE_PATH/site.path.postdeploy.server"
 touch "$GP_GIT_SITE_PATH/site.path.postdeploy.server"
+echo "Checking Parent folder ${GP_GIT_SITE_PATH}"
+ls -l  "$GP_GIT_SITE_PATH"
 
 echo "$GP_GIT_SITE_WEBROOT_PATH/site.webroot.path.postdeploy.server"
 touch "$GP_GIT_SITE_WEBROOT_PATH/site.webroot.path.postdeploy.server"
+echo "Checking Parent folder - ${GP_GIT_SITE_WEBROOT_PATH}"
+ls -l "$GP_GIT_SITE_WEBROOT_PATH"
 
