6746b994589f744bfad5fe0c3aa400072a68d49b
gridpane <36479264+gridpane@users.noreply.github.com>
2023-08-14 20:57:20 +0800

    Update predeploy.sh

--- .gpconfig/predeploy.sh
+++ .gpconfig/predeploy.sh
@@ -12,14 +12,27 @@ ls -l $GP_GIT_RELEASE_PATH
 
 echo "--------------------------"
 
-echo "In: $GP_GIT_SITE_WEBROOT_PATH"
-ls -l $GP_GIT_SITE_WEBROOT_PATH
+if [[ -n $GP_GIT_SITE_BUILD ]]; then
+
+  echo "This is a site build bind deploy.... "
+
+  echo "In: $GP_GIT_SITE_WEBROOT_PATH"
+  ls -l $GP_GIT_SITE_WEBROOT_PATH
+
+  echo "--------------------------"
+
+  echo "mkdir ${GP_GIT_SITE_PATH}/public/predeploy"
+  echo "touch ${GP_GIT_SITE_PATH}/public/predeploy/file"
+
+  mkdir -p ${GP_GIT_SITE_PATH}/public/predeploy
+  touch ${GP_GIT_SITE_PATH}/public/predeploy/file
+  ls -l ${GP_GIT_SITE_PATH}/public/predeploy
+
+else
+
+  echo "This is rebuild binds from code deploy"
+
+fi
 
 echo "--------------------------"
 
-echo "mkdir ${GP_GIT_SITE_PATH}/public/predeploy"
-echo "touch ${GP_GIT_SITE_PATH}/public/predeploy/file"
-
-mkdir -p ${GP_GIT_SITE_PATH}/public/predeploy
-touch ${GP_GIT_SITE_PATH}/public/predeploy/file
-ls -l ${GP_GIT_SITE_PATH}/public/predeploy
